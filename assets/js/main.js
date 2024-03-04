a7c6e2922f5fdeae5e1e121009650e637bf08feb
Michael Duarte <97355909+MichaelDu4rte@users.noreply.github.com>
2024-03-03 23:18:59 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  // faqs
+
+
+  const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
+
+
+// animation scrool
+
+window.addEventListener('scroll', reveal);
+
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal')
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active')
+    } 
+  }
+}
